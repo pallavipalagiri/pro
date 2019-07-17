5256c7a4e4ffa96799b5782ebde1947a1d7d5ee8
pallavipalagiri <52479205+pallavipalagiri@users.noreply.github.com>
2019-07-17 14:47:55 +0530

    Create 01.py

---
+++ 01.py
@@ -0,0 +1,12 @@
+p=int(input())
+b=[]
+for i in range(p):
+  c=input()
+  b.append(c)
+d=[]
+for i in zip(*b):
+  if(i.count(i[0])==len(i)):
+    d.append(i[0])
+  else:
+    break
+print(''.join(d))
