16710524ff4fef2d00f6655366d74d89ec1eb343
pallavipalagiri <52479205+pallavipalagiri@users.noreply.github.com>
2019-07-20 22:56:05 +0530

    Update 17.py

--- 17.py
+++ 17.py
@@ -1,10 +1,10 @@
-a,b=map(int,input().split())
+a,p=map(int,input().split())
 c=list(map(int,input().split()))[:a]
 d=0
 for i in range(a):
   for j in range(a):
     if(j>i):
-      if(c[i]+c[j]==b):
+      if(c[i]+c[j]==p):
         d+=1
 if(d>0):
   print("yes")
